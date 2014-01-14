0de7db4230777254541a7134c462648924ed6eae
blackcoat77 <kimanovic@gmail.com>
2014-01-14 17:43:52 +0100

    Adds css and theming in node 14, and adds new modules entity_translations and module filter

--- sites/all/themes/bootstrap_subtheme/templates/node--15.tpl.php
+++ sites/all/themes/bootstrap_subtheme/templates/node--15.tpl.php
@@ -1,69 +1,67 @@
 <table class="table table-bordered">
               <thead>
                 <tr>
-                    <th class="title-space"><?php
-                    print render($content['field_type']['#title']);?>
-                    </th>
+                  <th class="title-space">Le Type</th>
                   <td><?php print $type; ?></td>    
                 </tr>
               </thead>
                <tbody>  
                    
                    <tr>
-                     <th><?php print render($content['field_district']['#title']);?></th>
+                     <th>District</th>
                      <td><?php print $district;  ?></td>
                   </tr>
                 
                 <tr>
-                     <th><?php print render($content['field_location']['#title']);?></th>
+                     <th>Location</th>
                      <td><?php print $location; ?></td>
                 </tr>
                 
                 <tr>
-                      <th><?php print render($content['field_state']['#title']);?></th>
+                      <th>State</th>
                       <td><?php print $state; ?></td>
                 </tr>
                 
                 <tr>
-                      <th><?php print render($content['field_living_area']['#title']);?></th>
+                      <th>Living Area ( m² )</th>
                       <td><?php print $living_area; ?></td>
                 </tr>
                 
                <tr>
-                  <th><?php print render($content['field_land_area']['#title']);?></th>
+                  <th>Land Area ( m² )</th>
                   <td><?php print $land_area; ?></td>
                 </tr>
                 
                 
                 <tr>
-                  <th><?php print render($content['field_bedrooms']['#title']);?></th>
+                  <th>Bedrooms</th>
                   <td><?php print $bedrooms; ?></td>
                 </tr>
                 
                 
                <tr>
-                  <th><?php print render($content['field_baths']['#title']);?></th>
+                  <th>Baths</th>
                   <td><?php print $baths; ?></td>
                 </tr>
                 
                 <tr>
-                  <th><?php print render($content['field_parking_spots']['#title']);?></th>
+                  <th>Parking Spots</th>
                   <td><?php print $parking; ?></td>
                 </tr>
                 
                  <tr>
-                      <th><?php print render($content['field_from_shore']['#title']);?></th>
+                      <th>Distance From Shore ( m )</th>
                       <td><?php print $from_shore; ?></td>
                 </tr>
                 
                 <tr>
-                  <th><?php print render($content['field_description']['#title']);?></th>
+                  <th>Description</th>
                   <td><?php print $description; ?></td>
                 </tr>
   
                 <tr>
-                    <th><?php print render($content['field_house_plan']['#title']);?></th>
-                    <td colspan="2"><?php  print render($content['field_house_plan'])?></td>
+                    <th>House Plan *click on image to zoom in</th>
+                  <td colspan="2"><?php  print render($content['field_house_plan'])?></td>
                 </tr>
                 
               </tbody>
